f52a5e16d3100e3cbf8af01685c7764132d54516
mb <magnus.ben@gmail.com>
2016-04-24 22:56:33 +0200

    made the url file a parameter and an output directoy to keep the videos.  Also save the videos as their video id's

--- download_videos.py
+++ download_videos.py
@@ -1,11 +1,19 @@
 from __future__ import unicode_literals
 import youtube_dl
+from os import path
+import sys
 
-file = open("videourls.txt","r")
+
 ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	""" Just an example video
-	"""
-	for url in file.readlines():
-		print url
-		ydl.download([url])
+if __name__ == "__main__":
+	"""Usage python download_videos.py <url_file> <out_directory>"""
+	file = open(sys.argv[1],"r")
+	directory = sys.argv[2]
+	ydl_opts = {'output':'id'}
+	ydl_opts = {"outtmpl" : path.join(directory,r"%(id)s")}
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		""" Just an example video
+		"""
+		for url in file.readlines():
+			print url
+			ydl.download([url])
