37fa9df47ded89316139d78bdbc6d6706c336757
Sofia Broome <broome.sofia@gmail.com>
2016-04-24 21:53:13 +0200

    can download multiple files from txtfile with urls

--- download_videos.py
+++ download_videos.py
@@ -1,8 +1,11 @@
 from __future__ import unicode_literals
 import youtube_dl
 
+file = open("videourls.txt","r")
 ydl_opts = {}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 	""" Just an example video
 	"""
-	ydl.download(['https://www.youtube.com/watch?v=wcGhCdEmeUM'])
+	for url in file.readlines():
+		print url
+		ydl.download([url])
